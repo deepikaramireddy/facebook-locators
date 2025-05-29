5c16df7b0fcd926dfb0c75cac41ac39884724c6a
deepikaramireddy <151945678+deepikaramireddy@users.noreply.github.com>
2025-05-29 17:37:40 +0530

    Automating the Registration page using selenium WebDriver

--- src/test/java/com/facebook/RegistrationPage.java
+++ src/test/java/com/facebook/RegistrationPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+//Automating the Registration page using selenium WebDriver
 public class RegistrationPage {
 
     public static void main(String[] args) throws InterruptedException {
